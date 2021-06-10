232b613804b913752ed89d972251cc6a600a0008
Horacio Sacramento <horaciosacramento@hotmail.com>
2021-06-10 16:48:36 +0100

    Powershell example scripts

---
+++ GetEventIdListFromLog.ps1
@@ -0,0 +1,3 @@
+﻿# Example to get the list of a specific event id (6009) from a chosen log (System)
+
+Get-EventLog -LogName System | Where-Object {$_.EventID -eq 6009}
---
+++ RemoveInstalledApp.ps1
@@ -0,0 +1,13 @@
+﻿# Script example to remove Silverlight from a given list of machines
+
+$serverList = Get-Content "C:\Temp\serverList.txt"
+$appName = "Microsoft Silverlight"
+$accessAccount = Get-Credential
+
+ForEach ($serverName in $serverList) {
+    Invoke-Command -ComputerName $serverName -Credential $accessAccount -ScriptBlock {
+        Get-WmiObject Win32_product | Where {$_.name -eq $appName} | ForEach {
+            $_.Uninstall()
+        }
+    }
+}
